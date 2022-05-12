fb1ce01ce34c3913fbf65b1f0702d2feb80dc104
PetyoGD <dbassy123@gmail.com>
2022-05-12 20:36:13 +0300

    update

---
+++ src/main/java/forLoopEx/TennisRanklist.java
@@ -0,0 +1,39 @@
+package forLoopEx;
+
+import java.util.Scanner;
+
+public class TennisRanklist {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int ntournament = Integer.parseInt(scanner.nextLine());
+        int startPoints = Integer.parseInt(scanner.nextLine());
+        int w = 0;
+        int f = 0;
+        int sf = 0;
+        double wNumber = 0;
+        for (int i = 1; i <= ntournament; i++) {
+            String tournament = scanner.nextLine();
+            switch (tournament) {
+                case "W":
+                    wNumber++;
+                    int w1 = 2000;
+                    startPoints += w1;
+                    w += w1;
+                    break;
+                case "F":
+                    int f1 = 1200;
+                    startPoints += f1;
+                    f += f1;
+                    break;
+                case "SF":
+                    int sf1 = 720;
+                    startPoints += sf1;
+                    sf += sf1;
+                    break;
+            }
+        }
+        System.out.printf("Final points: %d%n", startPoints);
+        System.out.printf("Average points: %d%n", (w + f + sf) / ntournament);
+        System.out.printf("%.2f%%", (wNumber / ntournament) * 100);
+    }
+}
---
+++ src/main/java/forLoopEx/TrekkingMania.java
@@ -0,0 +1,58 @@
+package forLoopEx;
+
+import java.util.Scanner;
+
+public class TrekkingMania {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int nGroup = Integer.parseInt(scanner.nextLine());
+        double percent = 0;
+        double allPeople = 0;
+        double sumGroup1 = 0;
+        double sumGroup2 = 0;
+        double sumGroup3 = 0;
+        double sumGroup4 = 0;
+        double sumGroup5 = 0;
+        for (int i = 1; i <= nGroup; i++) {
+            int peoplesGroup = Integer.parseInt(scanner.nextLine());
+            allPeople += peoplesGroup;
+            if (peoplesGroup <= 5) {
+                sumGroup1 += peoplesGroup;
+            } else if (peoplesGroup <= 12) {
+                sumGroup2 += peoplesGroup;
+            } else if (peoplesGroup <= 25) {
+                sumGroup3 += peoplesGroup;
+            } else if (peoplesGroup <= 40) {
+                sumGroup4 += peoplesGroup;
+            } else {
+                sumGroup5 += peoplesGroup;
+            }
+        }
+        if (sumGroup1 == 0) {
+            System.out.println("0.00%");
+        } else {
+            System.out.printf("%.2f%%%n", (sumGroup1 / allPeople) * 100);
+        }
+        if (sumGroup2 == 0) {
+            System.out.println("0.00%");
+        } else {
+            System.out.printf("%.2f%%%n", (sumGroup2 / allPeople) * 100);
+        }
+        if (sumGroup3 == 0) {
+            System.out.println("0.00%");
+        } else {
+            System.out.printf("%.2f%%%n", (sumGroup3 / allPeople) * 100);
+        }
+        if (sumGroup4 == 0) {
+            System.out.println("0.00%");
+        } else {
+            System.out.printf("%.2f%%%n", (sumGroup4 / allPeople) * 100);
+        }
+        if (sumGroup5 == 0) {
+            System.out.println("0.00%");
+        } else {
+            System.out.printf("%.2f%%", (sumGroup5 / allPeople) * 100);
+        }
+    }
+}
